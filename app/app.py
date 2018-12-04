883596ea942ee6e818bc2f81f645be3270e99815
aratoval <chy.pawel@gmail.com>
2018-12-04 19:49:22 +0100

    reorganizacja funkcji i endpoints

--- app/app.py
+++ app/app.py
@@ -1,109 +1,110 @@
 #!/usr/bin/env  python3
-from flask import Flask
+from os import curdir
+from flask import Flask, jsonify, request, make_response
 from urllib import request as url_request
 from bs4 import BeautifulSoup
-from asyncio import async
+import threading
+import queue
 
 
 class ContentGetter:
 
-    current_status = ''
-    work_type = 0  # 0 - unknown, 1 - get text, 2 - get image, 3 - get all
+    work_type_dict = {0: "unknown", 1: "get text", 2: "get image", 3: "get all"}
+    current_status = ""
     workers_list = []
 
-    def __init__(self, url, work_type):
-        ContentGetter.workers_list.append(url)
-        self.id = url
-        self.url = url
-        self.status = "running"
+    def __init__(self, work_type, url):
+        ContentGetter.workers_list.append(id(self))
+        self.id = id(self)
         self.work_type = work_type
+        self.url = url
+        self.current_status = 'running'
+        # self.work()
 
-    def change_status(self):
-        self.current_status = 'finished'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "work_type": self.work_type,
+            "work_type_name": self.work_type_dict[int(self.work_type)],
+            "url": self.url,
+            "status": self.current_status
+        }
 
     def get_text(self):
-        page = url_request.urlopen(self.url)
-        content_page = page.read()
-        soup = BeautifulSoup(content_page, "html.parser")
-        print(soup.get_text())
-        return 0
+        try:
+            page = url_request.urlopen(self.url)
+            content_page = page.read()
+            soup = BeautifulSoup(content_page, "html.parser")
+            self.current_status = "finished"
+            return soup
+        except Exception as e:
+            self.current_status = "error: {}".format(e)
 
     def get_image(self):
-        page = url_request.urlopen(self.url)
-        content_page = page.read()
-        soup = BeautifulSoup(content_page, "html.parser")
-        image_list = soup.find_all('img')
-        print(image_list)
-        return 0
-
-    def work(self):
-        if self.work_type != 0:
-            if self.work_type == 1:
-                result = self.get_text()
-                if result == 0:
-                    self.change_status()
-            elif self.work_type == 2:
-                result = self.get_image()
-                if result == 0:
-                    self.change_status()
-            else:
-                result_1 = self.get_text()
-                result_2 = self.get_image()
-                if result_1 + result_2 == 0:
-                    self.change_status()
+        try:
+            page = url_request.urlopen(self.url)
+            content_page = page.read()
+            soup = BeautifulSoup(content_page, "html.parser")
+            image_list = soup.find_all('img')
+            self.current_status = "finished"
+            return image_list
+        except Exception as e:
+            self.current_status = "error: {}".format(e)
 
     def status(self):
-        print(self.current_status)
+        return {"status": self.current_status}
+
+    def work(self):
+        if self.work_type == 1:
+            self.get_text()
+        elif self.work_type == 2:
+            self.get_image()
+        else:
+            raise Exception("Incorrect work type")
 
 
 app = Flask(__name__)
 
 
-tasks_list = []
+tasks_dict = {}
 
 
 @app.route("/api/tasks", methods=["GET"])
-def tasks():
-    return str(ContentGetter.workers_list)
+def get_tasks():
+    return jsonify([tasks_dict[id].serialize() for id in tasks_dict.keys()])
 
 
-@app.route("/api/tasks/<task_type>/<url>", methods=["POST"])
-def run_tasks(task_type, url):
-    tasks_list.append(str(url))
-    tasks_list[-1] = ContentGetter(url, task_type)
-    return str(task_type) + url
+@app.route("/api/tasks/<int:id>", methods=["GET"])
+def get_tasks_of_id(id):
+    return jsonify(tasks_dict[int(id)].status())
 
 
-# # prosta aplikacja
-# @app.route("/", methods=['GET'])
-# def hello():
-#     return "Hello World!!"
-#
-#
-# # przekazanie parametru z URL
-# @app.route("/API/<name>")
-# def hello_who(name):
-#     return "Hello " + name + "!!"
-#
-#
-# # wyspecyfikowanie danych jakich sie oczekuje
-# @app.route("/age/<int:num>")
-# def age(num):
-#     return "You are " + num + " y.o."
-#
-#
-# # wskazanie by flask uzywal tez metody POST
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         # obsłuż dane logowania
-#         return "zalogowany"
-#     else:
-#         # wyświetl formularz logowania
-#         return "formularz"
+@app.route("/api/tasks/text", methods=["POST"])
+def create_task_text():
+    url = request.args.get("url")
+    a = ContentGetter(1, url)
+    t = threading.Thread(target=a.work())
+    t.setDaemon(True)
+    t.start()
+    tasks_dict[id(a)] = a
+    return jsonify(a.serialize())
+
+
+@app.route("/api/tasks/image", methods=["POST"])
+def create_task_image():
+    url = request.args.get("url")
+    a = ContentGetter(2, url)
+    t = threading.Thread(target=a.work())
+    t.setDaemon(True)
+    t.start()
+    tasks_dict[id(a)] = a
+    return jsonify(a.serialize())
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
-#    get_text()
-#    get_image()
